88b07a72d6c39c2a364b2643d2ed1360acdcdae4
Pankaj Lawande <pankaj.lawande@gmail.com>
2020-10-27 14:05:37 -0500

    First Commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from "react"
+import ReactDOM from "react-dom"
+//const element = <h1>Hello from Pankaj Lawande React learning App</h1>
+//ReactDom.render(element, document.getElementById("root"))
+// Component file
+import TodoContainer from "./components/TodoContainer"
+import "./App.css"
+ReactDOM.render(<TodoContainer/>, document.getElementById("root"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
