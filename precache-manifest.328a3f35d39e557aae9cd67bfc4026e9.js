9f91cc995d586409ee99ba38543ef1e213e318fa
Pankaj Lawande <pankaj.lawande@gmail.com>
2020-10-27 14:18:38 -0500

    Updates

---
+++ precache-manifest.328a3f35d39e557aae9cd67bfc4026e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a25bf07b491fd0e6c3d583b8cd084854",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "8a7341425addc7c994c5",
+    "url": "/todolist/static/css/main.247aee79.chunk.css"
+  },
+  {
+    "revision": "fd1d0adf2e2d5f2efda0",
+    "url": "/todolist/static/js/2.1baddac9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist/static/js/2.1baddac9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a7341425addc7c994c5",
+    "url": "/todolist/static/js/main.d04b238e.chunk.js"
+  },
+  {
+    "revision": "82da67e1da5d1bc4c571",
+    "url": "/todolist/static/js/runtime-main.054c540d.js"
+  }
+]);
